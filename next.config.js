bb25021857efa14e35448e926b878173cca5b3dd
Alexander Templeton <blakmatr@gmail.com>
2022-04-15 18:22:04 -0500

    unfuckin' webpack

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')()
 const runtimeCaching = require('next-pwa/cache')
 const withPWA = require('next-pwa')
 const withPlugins = require('next-compose-plugins')
 module.exports = withPlugins([
+  [withBundleAnalyzer, {
+    enabled: process.env.ANALYZE === 'false',
+    reactStrictMode: true,
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    distDir: 'build',
+  }],
   [withPWA, {
     pwa: {
       dest: 'public',
