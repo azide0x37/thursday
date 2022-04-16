2ece71df23aa65e0e9c57baac04b8e3efb11fce4
Alexander Templeton <blakmatr@gmail.com>
2022-04-15 23:56:15 -0500

    cleaning up inaccesable code that was split out to BGGGameListGen

--- amplify/backend/function/BGGGameById/src/index.py
+++ amplify/backend/function/BGGGameById/src/index.py
@@ -33,34 +33,6 @@ def GameDetailsById(id):
     except:
         return "error"
 
-
-def GameListGen(partialname):
-    try:
-        responsename = requests.get(BASE_URL+"/search?query="+partialname+"&type=boardgame")
-        tree = ET.fromstring(responsename.content)
-        x = '{}'
-        y = 0
-        namejson = json.loads(x)
-        # Due to convertion from XML iteration through nested valued is required. First level has the ID, second level has the name.
-        for child in tree:
-            for item in child:
-                if item.tag == 'name':
-                    # get the name from XML and transform into Json
-                    gamename = json.dumps(item.attrib)
-                    gamevalue = json.loads(gamename)
-                    # get the ID from previos level and format into Json
-                    gameid = json.dumps(child.attrib)
-                    gameidvalue = json.loads(gameid)
- #                   print(gameidvalue)
-                    thumb = json.dumps(GameDetailsById(gameidvalue['id']))
-                    thumbvalue = json.loads(thumb)
-                    formated = {"game"+str(y):{'name':str(gamevalue['value']), 'id':str(gameidvalue['id']), 'image': str(thumbvalue['image'])}}
-                    namejson.update(formated)                    
-                    y = y+1
-        return namejson
-    except:
-        return "error"
-
 def handler(event, context):
   print('received event:')
   print(event)
@@ -75,7 +47,3 @@ def handler(event, context):
       'body': GameDetailsById(id=event['board_game_id'])
   }
 
-
-###########Comment OUT when merging###################
-# if __name__ == '__main__':
-#    print(GameListGen('Terraforming Mars: ares'))
